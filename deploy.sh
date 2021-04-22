ea5343833a139e42df96d9b3cadb5348e8f75327
nikcich <nrc170001@utdallas.edu>
2021-04-22 04:27:44 +0000

    ree

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo "Building app"
 npm run build
 
 echo "Deploying files to server"
-rsync -avP build/ root@binder.codes:/var/www/binder.codes/
+rsync -avP build/ root@66.228.51.99:/var/www/66.228.51.99/
 echo "Deployment complete"
